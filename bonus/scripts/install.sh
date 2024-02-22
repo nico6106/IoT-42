a927b8102e8623429cb7398c69b2380d3c5b3cd5
Nicolas Lesage <nicolas.lesage@hotmail.fr>
2024-02-22 17:16:59 +0100

    adapt script with hosts and helm configuration

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -10,19 +10,19 @@ sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
 # curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 # sudo EXTERNAL_URL="http://my-host.internal" apt-get install gitlab-ee
 
-# HOST_ENTRY="127.0.0.1   my-host.internal"
-# HOST_SEARCH="my-host.internal"
-# HOST_FILE="/etc/hosts"
+HOST_ENTRY="127.0.0.1   my-host.internal"
+HOST_SEARCH="my-host.internal"
+HOST_FILE="/etc/hosts"
 
-# if grep -q "$HOST_SEARCH" "$HOST_FILE"; then
-#     echo "host exists"
-# else
-#     echo "$HOST_ENTRY" | sudo tee -a "$HOST_FILE"
-#     echo "host added"
-# fi
+if grep -q "$HOST_SEARCH" "$HOST_FILE"; then
+    echo "host exists"
+else
+    echo "$HOST_ENTRY" | sudo tee -a "$HOST_FILE"
+    echo "host added"
+fi
 
-# echo "MDP = "
-# sudo cat /etc/gitlab/initial_root_password
+echo "MDP = "
+sudo cat /etc/gitlab/initial_root_password
 
 #install helm
 if command_exists helm; then
@@ -65,10 +65,32 @@ else
     curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 fi
 
-#configure
+#configure k3d
 sudo k3d cluster create p3 --port "8888:31728@server:0"
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 sudo kubectl create namespace gitlab
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+#configure helm
+sudo helm repo add gitlab https://charts.gitlab.io/
+sudo helm repo update
+sudo helm upgrade --install gitlab gitlab/gitlab \
+    -n gitlab \
+    -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+    --set global.hosts.domain=my-host.internal \
+    --set global.hosts.externalIP=0.0.0.0 \
+    --set global.hosts.https=false \
+    --timeout 600s
+
+#waiting for webservice to be running
+sudo kubectl wait --for=condition=ready --timeout=1200s pod -l app=webservice -n gitlab
+
+#get PWD for gitlab
+echo -n "GITLAB PASSWORD : "
+sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode
+
+# argocd localhost:80 or http://my-host.internal
+sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 2>&1 >/dev/null &
+
