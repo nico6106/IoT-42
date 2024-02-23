99f46352e211b3444758c9e9e83ce2e57eb6d9d8
Nicolas Lesage <nicolas.lesage@hotmail.fr>
2024-02-23 11:53:19 +0100

    adding sudo

--- bonus/scripts/launch.sh
+++ bonus/scripts/launch.sh
@@ -6,11 +6,11 @@ error_exit() {
 }
 
 echo "Creating k3d cluster..."
-k3d cluster create bonus --port "8888:31728@server:0" || error_exit "Failed to create k3d cluster"
+sudo k3d cluster create bonus --port "8888:31728@server:0" || error_exit "Failed to create k3d cluster"
 
 echo "Deploying ArgoCD and application..."
-kubectl create namespace argocd || error_exit "Failed to create namespace 'argocd'"
-kubectl create namespace dev || error_exit "Failed to create namespace 'dev'"
+sudo kubectl create namespace argocd || error_exit "Failed to create namespace 'argocd'"
+sudo kubectl create namespace dev || error_exit "Failed to create namespace 'dev'"
 sudo kubectl create namespace gitlab || error_exit "Failed to create namespace 'gitlab'"
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || error_exit "Failed to apply ArgoCD manifest"
